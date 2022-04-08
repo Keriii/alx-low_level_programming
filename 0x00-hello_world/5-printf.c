2555a2a67994aa626f7db8ea00df0c6e8a8b6880
keriii <kerod53@gmail.com>
2022-04-08 07:37:27 -0700

    hopfully my last correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("With proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return(0);
 }
