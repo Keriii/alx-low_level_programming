ca1bfcc7973e5a1fa1fcfd38289e3313877d2ed3
keriii <kerod53@gmail.com>
2022-04-08 13:51:44 -0700

    am

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,14 @@
  * Description: Print lowercase and uppercase alphabet on the same line
  * Return 0
  */
-int main (void)
+int main(void)
 {
-	int c;
+int c;
 
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
 	for (c = 0; c < 26; ++c)
-		putchar('a' +c);
-	for (c = 0, c < 26; ++c)
-		putchar('A' +c);
+		putchar('A' + c);
 
 	putchar('\n');
 
