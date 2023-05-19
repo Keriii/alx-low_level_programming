291896dcb4b31fd5e944b74c06736bf58096f4d6
Kerod Sisay <kerod53@gmail.com>
2023-05-19 07:57:15 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * mian - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
