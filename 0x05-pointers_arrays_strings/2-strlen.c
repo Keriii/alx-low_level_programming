cc4d70f5e0831f642209a1c91bc74b4f1c73c231
Kerod Sisay <kerod53@gmail.com>
2023-05-30 15:00:31 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of astring
+ * @s: integer 
+ * Return: 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*(s + a) != '\0')
+		count++;
+	return (a);
+}
