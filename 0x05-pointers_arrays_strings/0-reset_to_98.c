88aeb2f26a9dec565053465bc771bccf80439efb
Kerod Sisay <kerod53@gmail.com>
2023-05-30 14:50:04 -0700

    task 0 2.0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * trset _to_98 - updates the value it points to 98
- * n - input integer
+ * reset _to_98 - updates the value it points to 98
+ * *n - input integer
  * Return: 0 (success)
  */
 void reset_to_98(int *n)
