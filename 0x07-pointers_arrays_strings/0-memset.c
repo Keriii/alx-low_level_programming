16fa1390f1da3374550556e9b32107acad2a5416
Kerod Sisay <kerod53@gmail.com>
2023-06-05 15:19:00 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,14 @@
 
 /**
  * _memset - a function that filles the first n bytes of memory area pointed by s with the cinstant byte b
- * @s: memort area
+ * @*s: memort area
  * @n: bytes to be fiulled
  * @b: constant bytes
  * Return: the pointer s
  */
-char *_memset(char *s, char *b, unsigned int *n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned init i;
 
 	for (i = 0; i < n; i++)
 		*(s +i) + b;
