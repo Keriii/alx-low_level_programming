960f21974a35e8713e34f3e998412843220f9efd
Kerod Sisay <kerod53@gmail.com>
2023-06-12 03:21:03 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a;
+	
+	for (a =97; a <= 122; a++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+return (0);
+}
