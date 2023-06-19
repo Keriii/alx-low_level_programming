b9f8362a7655e3d29d1e2d8820c3956eeb50b51c
Kerod Sisay <kerod53@gmail.com>
2023-06-19 13:22:18 -0700

    tas k1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initializes a variable
+ * @name: character
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * @d: struct
+ *
+ * Return: 0 (Success)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d)
+	{
+		d.name =name;
+		d.age = age;
+		d.owner = owner;
+	}
+}
