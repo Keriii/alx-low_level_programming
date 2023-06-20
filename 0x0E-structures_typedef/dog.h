7796a81b47c4228a455508f1662e9cd830670ef2
Kerod Sisay <kerod53@gmail.com>
2023-06-20 03:36:08 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-#ifdef _DOG_H_
-#define _DOG_H_
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
