075c13dfa159fa8b6611de60337763e3b91d61b8
Kerod Sisay <kerod53@gmail.com>
2023-06-20 03:52:51 -0700

    task 1 corrected

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,12 @@
  * Return: 0 (Success)
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d.name =name;
-		d.age = age;
-		d.owner = owner;
+		d->name =name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
