a35faa6d9460bc7d9d717e6a8673f026f7e61526
Kerod Sisay <kerod53@gmail.com>
2023-06-21 03:10:14 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function given as a parameter on each element of an array.
+ * @array: array
+ * @size: size of the array
+ * @action: pointer function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a;
+
+	if (array && action)
+		for  (a = 0; a < size; a++)
+			action(array[a]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer
+ * @array: array
+ * @size: size of array
+ * @cmp: a pointer to the function
+ * Return: no element matches, return -1 If size <= 0, return -1 or 0 for nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array &&cmp)
+	{
+	if (size <= 0)
+	{
+		return (0);
+	}
+	if (size > 0)
+	{
+		for (a = 0 ; a < size; a++)
+			if(cmp(array[a]))
+				return (i);
+	}
+	}
+
+return (-1);
+	}
